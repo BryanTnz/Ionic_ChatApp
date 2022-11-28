0743f234067d5fb11b39b4821878ec0114dad01e
myportafoliobryantnzapp <bry.tandazo@gmail.com>
2022-11-27 19:45:27 -0500

    Trabajo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,18 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "AIzaSyCScJ6yqrkBvfQfrwFurY0uNAjGtK-gE6Y",
-    authDomain: "examenbimestralte.firebaseapp.com",
-    projectId: "examenbimestralte",
-    storageBucket: "examenbimestralte.appspot.com",
-    messagingSenderId: "373837409542",
-    appId: "1:373837409542:web:adb70c261ec5a9bd350287"
+    // apiKey: "AIzaSyCScJ6yqrkBvfQfrwFurY0uNAjGtK-gE6Y",
+    // authDomain: "examenbimestralte.firebaseapp.com",
+    // projectId: "examenbimestralte",
+    // storageBucket: "examenbimestralte.appspot.com",
+    // messagingSenderId: "373837409542",
+    // appId: "1:373837409542:web:adb70c261ec5a9bd350287"
+    apiKey: "AIzaSyAx_zu8AacA1syq-k8iRK-LfpovRW4K_Jw",
+    authDomain: "appmoviles-6c126.firebaseapp.com",
+    projectId: "appmoviles-6c126",
+    storageBucket: "appmoviles-6c126.appspot.com",
+    messagingSenderId: "697687320846",
+    appId: "1:697687320846:web:6e9cb17746ed6072e24f5f"
 
 
   }};
